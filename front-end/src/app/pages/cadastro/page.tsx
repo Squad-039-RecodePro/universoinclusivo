e1e1b7f35d8cec0d40478971a33a1931bdbf6ef4
Thiagoqdev <Thiagoq.dev@gmail.com>
2024-01-25 23:48:18 -0300

    adicionado frontend em next

---
+++ front-end/src/app/pages/cadastro/page.tsx
@@ -0,0 +1,78 @@
+import "./cadastro.css";
+
+export default function Cadastro() {
+  return (
+    <main>
+      <div className="container container-custom mx-auto border my-5">
+        <div>
+          <h1 className="text-center mt-4 mb-3">Conecte-se agora</h1>
+          <p className="text-center mt-2 mb-4">Olá, Bem-vindo de volta &#128075; </p>
+        </div>
+        <div className="text-center">
+          <button className="btn btn btn-dark">
+            <img
+              src="/img/icon-google.png"
+              alt="icone do google"
+              className="me-3"
+            />
+            Faça login com o Google
+          </button>
+        </div>
+        <p className="textLogin-cor text-center mt-4"> Faça login com o Google</p>
+
+  <form className="col-sm-12 col-md-8 col-lg-6 mx-auto">
+    <div className="form-group input-group mb-3">
+      <label htmlFor="login" className="w-100">
+        Email
+        <input
+          className="form-control"
+          name="login"
+          id="login"
+          type="text"
+          placeholder="Digite seu email"
+        />
+      </label>
+    </div>
+    <div className="form-group input-group mb-3">
+      <label htmlFor="senha" className="w-100">
+        Senha
+        <input
+          className="form-control"
+          name="senha"
+          id="senha"
+          type="password"
+
+          placeholder="Digite sua senha"
+        />
+      </label>
+    </div>
+  </form>
+
+        <div className="d-flex justify-content-evenly mt-4">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              defaultValue=""
+              id="flexCheckIndeterminate"
+            />
+            <label className="form-check-label" htmlFor="LembreDeMim">
+              Lembre de mim
+            </label>
+          </div>
+
+          <a href="#">
+            <p> Esqueceu a senha ?</p>
+          </a>
+        </div>
+
+        <div className="d-grid gap-2 col-6 mx-auto mt-3 mb-4">
+          <button className="btn btn-danger mb-3" type="button">
+            Conecte-se
+          </button>
+          <p className="text-center">Não é registrado ainda ? <br></br> <a href="#">Crie uma nova conta</a></p> 
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ front-end/src/app/pages/contato/page.tsx
@@ -0,0 +1,102 @@
+'use client'
+
+export default function Contato() {
+
+  return (
+    <main>
+      <section className="contact-section section_padding">
+
+        <h1 className="text-center mt-5">
+          CONTATO
+        </h1>
+        <div className="container_contato container mt-5">
+          <div className="row">
+            <div className="col-12">
+              <h2 className="contact-title">Entrar em Contato</h2>
+            </div>
+            <div className="col-lg-8">
+              <form className="form-contact contact_form" action="contact_process.php" method="post"
+                id="contactForm" noValidate={true}>
+                <div className="row">
+                  <div className="col-12">
+                    <div className="form-group">
+
+                      <textarea className="form-control input-group mb-3 w-100" name="message" id="message" 
+                        cols={30} rows={9} onFocus={(e) => e.target.placeholder = ''}
+                        onBlur={(e) => e.target.placeholder = 'Enter Message'}
+                        placeholder='Digite a Mensagem'></textarea>
+                    </div>
+                  </div>
+                  <div className="col-sm-6">
+                    <div className="form-group input-group mb-3">
+                      <input className="form-control" name="name" id="name" type="text"
+                        onFocus={(e) => e.target.placeholder = ''}
+                        onBlur={(e) => e.target.placeholder = 'Enter your name'} 
+                        placeholder='Digite seu Nome' />
+                    </div>
+                  </div>
+                  <div className="col-sm-6">
+                    <div className="form-group">
+                      <input className="form-control input-group mb-3" name="email" id="email" type="email"
+                        onFocus={(e) => e.target.placeholder = ''}
+                        onBlur={(e) => e.target.placeholder = 'Enter email address'}
+                        placeholder='Insira seu Email' />
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-group">
+                      <input className="form-control" name="subject" id="subject" type="text"
+                        onFocus={(e) => e.target.placeholder = ''} 
+                        onBlur={(e) => e.target.placeholder = 'Enter Subject'}
+                        placeholder='Digite o Assunto' />
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group mt-3">
+                  <button type="submit" className=" mt-3 button btn btn-info button-contactForm btn_1">Enviar
+                    Mensagem</button>
+                </div>
+              </form>
+            </div>
+            <div className=" mt-5 col-lg-4">
+              <div className="media contact-info">
+                <span className="contact-info__icon"><i className="ti-home"></i></span>
+                <div className="media-body">
+                  <h3>Rio de Janeiro</h3>
+                  <p>Rua Presidente Vargas, 123</p>
+                </div>
+              </div>
+              <div className="media contact-info">
+                <span className="contact-info__icon"><i className="ti-tablet"></i></span>
+                <div className="media-body">
+                  <h3>(21) 3456-45678</h3>
+                  <p>De Seg-Sexta | 08h às 18h</p>
+                </div>
+              </div>
+              <div className="media contact-info">
+                <span className="contact-info__icon"><i className="ti-email"></i></span>
+                <div className="media-body">
+                  <h3>squad39recodepro@gmail.com</h3>
+                  <p>Envie-nos a sua consulta a qualquer momento!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section>
+        <div className="d-none d-sm-block mb-6 pb-4 container mt-5">
+          <h3 className="text-center">LOCALIZAÇÃO</h3>
+          <iframe className="mt-4"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235199.37769568487!2d-43.610790246516785!3d-22.913729487149148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bde559108a05b%3A0x50dc426c672fd24e!2sRio%20de%20Janeiro%2C%20RJ!5e0!3m2!1spt-BR!2sbr!4v1692966732739!5m2!1spt-BR!2sbr"
+            width="100%" height="450" style={{border:"0"}} allowFullScreen={true} loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </section>
+
+    </main>
+
+  );
+}
